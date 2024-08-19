f52290f452ce95c1af7524377622fa11b54cb85a
Paula <paulamurta@hotmail.com>
2024-08-19 05:45:39 -0400

    fix: Add localhost to service

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://192.168.0.9:3009/api",
+  baseURL: "http://localhost:3009/api",
 });
 
 api.interceptors.request.use((config: any) => {
